2d876420f5d2d79938309320c7fdf8d3d0013b98
twygw <stanley.dix@sky.uk>
2023-11-09 15:29:38 +0000

    musicinas router

--- routes/musicians.js
+++ routes/musicians.js
@@ -4,5 +4,23 @@ const router = express.Router();
 router.use("/musicians", (req, res, next) => {
   next();
 });
+router.set("json spaces", 2);
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+router.get("/musicians", async (req, res) => {
+  const musicians = await Musician.findAll();
+  res.json(musicians);
+});
+
+router.get("/musicians/:id", async (req, res) => {
+  const id = req.params.id;
+  const musicians = await Musician.findByPk(id);
+  res.json(musicians);
+});
+router.post("/musicians", async (req, res) => {
+  const newMusican = await Musician.create(req.body);
+  res.json(newMusican);
+});
 
 module.exports = router;
